1a747b02f7edcef98e3bb8bf6431bd7acd9e1b33
David Ko <dko@suse.com>
2020-08-27 10:51:57 +0800

    Fix #14 invalid ctr-tool version selected when deploying kubeadm/1.19.0

--- pkg/util/github.go
+++ pkg/util/github.go
@@ -6,7 +6,6 @@ import (
 	"github.com/innobead/kubefire/pkg/data"
 	"github.com/pkg/errors"
 	"strconv"
-	"strings"
 )
 
 type GithubInfoer struct {
@@ -21,7 +20,6 @@ func (g *GithubInfoer) GetVersionsAfterVersion(afterVersion data.Version, repoOw
 	var versions []*data.Version
 
 	opt := github.ListOptions{}
-	lastMajorVersion := afterVersion.Major
 done:
 	for {
 		releases, resp, err := g.client.Repositories.ListReleases(context.Background(), repoOwner, repo, &opt)
@@ -30,32 +28,35 @@ done:
 		}
 
 		for _, release := range releases {
-			if lastMajorVersion != afterVersion.Major {
-				if !strings.HasPrefix(release.GetTagName(), afterVersion.MajorString()+".") {
-					afterVersion.Minor = data.ParseVersion(release.GetTagName()).Minor
+			releaseVersion := data.ParseVersion(release.GetTagName())
+			if releaseVersion == nil {
+				continue
+			}
+
+			if releaseVersion.MajorString() != afterVersion.MajorString() {
+				afterVersion.Major = data.SubVersionType(strconv.Itoa(afterVersion.Major.ToInt() - 1))
+				afterVersion.Minor = data.SubVersionType(strconv.Itoa(100))
+
+				continue
+			}
+
+			if releaseVersion.MajorMinorString() != afterVersion.MajorMinorString() {
+				if afterVersion.Minor.ToInt()-1 < 0 {
+					return nil, errors.New("unexpected error, out of range of minor versions")
+				}
+
+				afterVersion.Minor = data.SubVersionType(strconv.Itoa(afterVersion.Minor.ToInt() - 1))
+
+				if releaseVersion.MajorMinorString() != afterVersion.MajorMinorString() {
+					continue
 				}
 			}
 
-			if !strings.HasPrefix(release.GetTagName(), afterVersion.MajorMinorString()+".") {
-				continue
-			}
-
-			v := data.ParseVersion(release.GetTagName())
-			if v == nil {
-				continue
-			}
-			versions = append(versions, v)
+			versions = append(versions, releaseVersion)
 
 			minorVersionCount--
 			if minorVersionCount == 0 {
 				break done
-			}
-
-			if afterVersion.Minor.ToInt()-1 < 0 {
-				// FIXME how to deal with the minor version when major version changes like from v2 to v1
-				afterVersion.Major = data.SubVersionType(strconv.Itoa(afterVersion.Major.ToInt() - 1))
-			} else {
-				afterVersion.Minor = data.SubVersionType(strconv.Itoa(afterVersion.Minor.ToInt() - 1))
 			}
 		}
 
