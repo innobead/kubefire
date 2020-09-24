9dec3d752c5ebdd9996b3a752fe922bc2d57ccfb
David Ko <dko@suse.com>
2020-09-24 21:35:58 +0800

    Add error check for some curl downloads

--- hack/install-release-kubefire.sh
+++ hack/install-release-kubefire.sh
@@ -6,9 +6,9 @@ set -o pipefail
 set -o xtrace
 
 function get_latest_release() {
-  curl -sSL "https://api.github.com/repos/innobead/kubefire/releases/latest" |
+  curl -sfSL "https://api.github.com/repos/innobead/kubefire/releases/latest" |
     grep '"tag_name":' |
     sed -E 's/.*"([^"]+)".*/\1/'
 }
 
-curl -sSLO "https://github.com/innobead/kubefire/releases/download/$(get_latest_release)/kubefire" && chmod +x kubefire && sudo mv kubefire /usr/local/bin
+curl -sfSLO "https://github.com/innobead/kubefire/releases/download/$(get_latest_release)/kubefire" && chmod +x kubefire && sudo mv kubefire /usr/local/bin
