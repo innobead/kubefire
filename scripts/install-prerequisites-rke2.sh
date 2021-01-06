8c22d3cec7b34e9615b49236544c16b302ef331c
David Ko <dko@suse.com>
2021-01-07 01:17:18 +0800

    Fix #41 add rke2 install script

---
+++ scripts/install-prerequisites-rke2.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+TMP_DIR=/tmp/kubefire
+
+RKE2_VERSION=${RKE2_VERSION:-}
+RKE2_CONFIG=${RKE2_CONFIG:-}
+
+if [ -z "$RKE2_VERSION" ]; then
+  echo "incorrect versions provided!" >/dev/stderr
+  exit 1
+fi
+
+rm -rf $TMP_DIR && mkdir -p $TMP_DIR
+pushd $TMP_DIR
+
+function cleanup() {
+  rm -rf $TMP_DIR || true
+  popd
+}
+
+trap cleanup EXIT ERR INT TERM
+
+function install_rke2() {
+  # https://get.rke2.io
+  local url="https://raw.githubusercontent.com/rancher/rke2/${RKE2_VERSION}/install.sh"
+  curl -sfSL "$url" -o rke2-install.sh
+  chmod +x rke2-install.sh && sudo mv rke2-install.sh /usr/local/bin/
+}
+
+function create_rke2_config() {
+  if [ -n "$RKE2_CONFIG" ]; then
+    mkdir -p /etc/rancher/rke2 || true
+    echo "$RKE2_CONFIG" >/etc/rancher/rke2/config.yaml
+  fi
+}
+
+$1
+
